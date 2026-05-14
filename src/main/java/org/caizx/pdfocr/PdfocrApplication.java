f3ded09ebca6ee6f456917e2e5c47d3661dc77af
admin <caizzx@cug.cn>
2026-05-14 15:02:09 +0800

    实现视觉模型解析pdf

--- src/main/java/org/caizx/pdfocr/PdfocrApplication.java
+++ src/main/java/org/caizx/pdfocr/PdfocrApplication.java
@@ -2,12 +2,13 @@ package org.caizx.pdfocr;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class PdfocrApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(PdfocrApplication.class, args);
     }
-
 }
